fe37da42c5f043d9eef3186069e387ff896e6cd8
屈文斌 <quwenbin@blchina.com>
2022-06-13 15:21:57 +0800

    dns name.rs完善

--- pkg/dns/name/src/name.rs
+++ pkg/dns/name/src/name.rs
@@ -11,7 +11,7 @@ pub struct Name(String);
 /// A reference to a DNS Name suitable for use in the TLS Server Name Indication
 /// (SNI) extension and/or for use as the reference hostname for which to verify
 /// a certificate.
-#[derive(Clone, Copy, Debug, Eq, )]
+#[derive(Clone, Copy, Debug, Eq, Hash)]
 pub struct NameRef<'a>(&'a str);
 
 #[derive(Copy, Clone, Debug, Eq, PartialEq, Error)]
@@ -91,6 +91,28 @@ impl<'a> NameRef<'a> {
     /// Constructs a `Name` from this `NameRef`
     pub fn to_owned(self) -> Name {
         Name(self.as_str().to_ascii_lowercase())
+    }
+
+    #[inline]
+    pub fn as_str(&self) -> &str {
+        self.0
+    }
+
+    #[inline]
+    pub fn as_bytes(&self) -> &[u8] {
+        self.0.as_bytes()
+    }
+}
+
+impl<'a> PartialEq<NameRef<'a>> for NameRef<'_> {
+    fn eq(&self, other: &NameRef<'a>) -> bool {
+        self.0.eq_ignore_ascii_case(other.0)
+    }
+}
+
+impl fmt::Display for NameRef<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> Result<(), fmt::Error> {
+        self.as_str().fmt(f)
     }
 }
 
@@ -186,7 +208,104 @@ fn is_vaild__dns_id(hostname: untrusted::Input<'_>) -> bool {
 mod tests {
     use super::*;
 
+    #[test]
     fn test_parse() {
-        todo!()
+        const CASES: &[(&str, bool)] = &[
+            ("", false),
+            (".", false),
+            ("..", false),
+            ("...", false),
+            ("*", false),
+            ("a", true),
+            ("a.", true),
+            ("d.c.b.a", true),
+            ("d.c.b.a.", true),
+            (" d.c.b.a.", false),
+            ("d.c.b.a-", false),
+            ("*.a.", false),
+            (".a.", false),
+            ("a1", true),
+            ("_m.foo.bar", true),
+            ("m.foo.bar_", true),
+            ("example.com:80", false),
+            ("1", false),
+            ("1.a", true),
+            ("a.1", false),
+            ("1.2.3.4", false),
+            ("::1", false),
+            ("xn--poema-9qae5a.com.br", true), // IDN
+        ];
+
+        for &(n, expected_result) in CASES {
+            assert!(n.parse::<Name>().is_ok() == expected_result, "{}", n);
+        }
+    }
+
+    #[test]
+    fn test_eq() {
+        const CASES: &[(&str, &str, bool)] = &[
+            ("a", "a", true),
+            ("a", "b", false),
+            ("d.c.b.a", "d.c.b.a", true),
+            // case sensitivity
+            (
+                "abcdefghijklmnopqrstuvwxyz",
+                "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+                true,
+            ),
+            ("aBc", "Abc", true),
+            ("a1", "A1", true),
+            ("example", "example", true),
+            ("example.", "example.", true),
+            ("example", "example.", false),
+            ("example.com", "example.com", true),
+            ("example.com.", "example.com.", true),
+            ("example.com", "example.com.", false),
+        ];
+        for &(left, right, expected_result) in CASES {
+            let l = left.parse::<Name>().unwrap();
+            let r = right.parse::<Name>().unwrap();
+            assert_eq!(l == r, expected_result, "{:?} vs {:?}", l, r);
+        }
+    }
+
+    #[test]
+    fn test_is_localhost() {
+        let cases = &[
+            ("localhost", false), // Not absolute
+            ("localhost.", true),
+            ("LocalhOsT.", true),   // Case-insensitive
+            ("mlocalhost.", false), // prefixed
+            ("localhost1.", false), // suffixed
+        ];
+        for (host, expected_result) in cases {
+            let dns_name = host.parse::<Name>().unwrap();
+            assert_eq!(dns_name.is_localhost(), *expected_result, "{:?}", dns_name)
+        }
+    }
+
+    #[test]
+    fn test_without_trailing_dot() {
+        let cases = &[
+            ("localhost", "localhost"),
+            ("localhost.", "localhost"),
+            ("LocalhOsT.", "localhost"),
+            ("web.svc.local", "web.svc.local"),
+            ("web.svc.local.", "web.svc.local"),
+        ];
+        for (host, expected_result) in cases {
+            let dns_name = host
+                .parse::<Name>()
+                .unwrap_or_else(|_| panic!("'{}' was invalid", host));
+            assert_eq!(
+                dns_name.without_trailing_dot(),
+                *expected_result,
+                "{:?}",
+                dns_name
+            )
+        }
+        assert!(".".parse::<Name>().is_err());
+        assert!("..".parse::<Name>().is_err());
+        assert!("".parse::<Name>().is_err());
     }
 }
