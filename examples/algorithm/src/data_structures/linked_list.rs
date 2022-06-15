7bb4ef63e9e3f66f02ce9df7f6624f526ac32138
屈文斌 <quwenbin@blchina.com>
2022-06-15 11:43:08 +0800

    linked_list.rs

--- examples/algorithm/src/data_structures/linked_list.rs
+++ examples/algorithm/src/data_structures/linked_list.rs
@@ -1,3 +1,10 @@
+/*
+    双向链表
+    新知识点：
+    1、std::ptr::NonNull
+    2、std::marker::PhantomData
+*/
+use std::marker::PhantomData;
 use std::ptr::NonNull;
 
 struct Node<T> {
@@ -13,5 +20,56 @@ impl<T> Node<T> {
             next: None,
             perv: None,
         }
+    }
+}
+
+pub struct LinkedList<T> {
+    length: u32,
+    head: Option<NonNull<Node<T>>>,
+    tail: Option<NonNull<Node<T>>>,
+    market: PhantomData<Box<Node<T>>>,
+}
+
+impl<T> Default for LinkedList<T> {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl<T> LinkedList<T> {
+    pub fn new() -> Self {
+        Self {
+            length: 0,
+            head: None,
+            tail: None,
+            market: PhantomData,
+        }
+    }
+
+    pub fn insert_at_head(&mut self, obj: T) {
+        let mut node = Box::new(Node::new(obj));
+        node.next = self.head;
+        node.perv = None;
+
+        let node_ptr = Some(unsafe { NonNull::new_unchecked(Box::into_raw(node)) });
+        match self.head {
+            None => self.tail = node_ptr,
+            Some(head_ptr) => unsafe { (*head_ptr.as_ptr()).perv = node_ptr },
+        }
+        self.head = node_ptr;
+        self.length += 1;
+    }
+
+    pub fn insert_as_tail(&mut self, obj: T) {
+        let mut node = Box::new(Node::new(obj));
+        node.perv = self.tail;
+        node.next = None;
+        let node_ptr = Some(unsafe { NonNull::new_unchecked(Box::into_raw(node)) });
+        match self.tail {
+            None => self.head = node_ptr,
+            Some(tail_ptr) => unsafe { (*tail_ptr.as_ptr()).next = node_ptr },
+        }
+        self.tail = node_ptr;
+        self.length += 1;
     }
 }
