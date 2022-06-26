ddd55bc259c62378f2a60df92ba52ea518c2de27
wenbin <twfx7758@163.com>
2022-06-26 09:18:08 +0800

    proxies/pkg/error

--- pkg/dns/src/lib.rs
+++ pkg/dns/src/lib.rs
@@ -1,3 +1,15 @@
+use trust_dns_resolver::config::ResolverOpts;
+use trust_dns_resolver::TokioAsyncResolver;
+
+#[derive(Clone)]
+pub struct Resolver{
+    dns: TokioAsyncResolver,
+}
+
+pub trait ConfigureResolver {
+    fn configure_resolver(&self, _: &mut ResolverOpts);
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
