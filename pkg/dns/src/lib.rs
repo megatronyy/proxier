8ab735258ad1fdb357de31b9944ea9f5faf88680
屈文斌 <quwenbin@blchina.com>
2022-06-27 10:56:49 +0800

    proxies/pkg/errno

--- pkg/dns/src/lib.rs
+++ pkg/dns/src/lib.rs
@@ -1,14 +1,23 @@
-use trust_dns_resolver::config::ResolverOpts;
+use trust_dns_resolver::{config::ResolverOpts, error};
+use trust_dns_resolver::proto::rr::rdata;
 use trust_dns_resolver::TokioAsyncResolver;
 
 #[derive(Clone)]
-pub struct Resolver{
+pub struct Resolver {
     dns: TokioAsyncResolver,
 }
 
 pub trait ConfigureResolver {
     fn configure_resolver(&self, _: &mut ResolverOpts);
 }
+
+#[derive(Debug, Clone, Error)]
+#[error("invalid SRV record {:?}", self.0)]
+struct InvalidSrv(rdata::SRV);
+
+#[derive(Debug, Error)]
+#[error("failed to resolve A record: {0}")]
+struct ARecordError(#[from] error::ResolveError);
 
 #[cfg(test)]
 mod tests {
