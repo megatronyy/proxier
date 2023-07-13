29e79e7d9c40ad50d5dc55e7e00ea3f67668f990
屈文斌 <quwenbin@blchina.com>
2023-07-14 07:53:45 +0800

    研究rust的内部可变性， Cell/RefCell/RefMut/UnsafeCell

--- examples/basics/src/std_cells.rs
+++ examples/basics/src/std_cells.rs
@@ -1,13 +1,15 @@
-use std::cell::Cell;
+use std::cell::{Cell, RefCell, RefMut};
+use std::collections::HashMap;
+use std::rc::Rc;
 
 struct Foo {
     x: u32,
     y: Cell<u32>,
 }
 
-struct Foo2{
+struct Foo2 {
     x: u32,
-    y: u32
+    y: u32,
 }
 
 #[test]
@@ -24,4 +26,25 @@ fn test_foo2() {
     let foo = Foo2 { x: 1, y: 3 };
     assert_eq!(1, foo.x);
     assert_eq!(3, foo.y);
+}
+
+fn refcell_demo() {
+    let shared_map: Rc<RefCell<_>> = Rc::new(RefCell::new(HashMap::new()));
+
+    // create a new block to limit the scope of the dynamic borrow
+    {
+        let mut map: RefMut<'_, _> = shared_map.borrow_mut();
+        map.insert("africa", 92355);
+        map.insert("kyoto", 11837);
+        map.insert("piccadilly", 11826);
+        map.insert("marbles", 38);
+    }
+
+    let total: i32 = shared_map.borrow().values().sum();
+    println!("{}", total);
+}
+
+#[test]
+fn test_refcell_demo(){
+    refcell_demo();
 }
